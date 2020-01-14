e64a7a3bea09a8c1058a7f37185b715aa258f0b4
Chris McCauley <Chris.McCauley@davistech.edu>
2020-01-14 10:59:48 -0700

    added required imports

--- itProAuto.py
+++ itProAuto.py
@@ -1,8 +1,13 @@
 # http://kazuar.github.io/scraping-tutorial/
 
+import os
 import sys
 
-sys.path.insert(0, '/packages/')
+pkgPath = '{}{}packages{}'.format(os.getcwd(), os.path.sep, os.path.sep)
 
+sys.path.insert(0, pkgPath)
 
+import requests
+
+sessionRequests = requests.session()
 
